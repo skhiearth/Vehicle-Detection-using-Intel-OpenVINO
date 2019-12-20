827be60a1ca3dfade8b427f83178e5bce2efc381
skhiearth <skhiearth@gmail.com>
2019-12-21 01:49:21 +0530

    create py files

---
+++ app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 21 01:44:50 2019
+
+@author: skhiearth
+"""
+
---
+++ inference.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 21 01:45:04 2019
+
+@author: skhiearth
+"""
+
