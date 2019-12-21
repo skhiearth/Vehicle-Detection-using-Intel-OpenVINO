66b565ad73afbbc2849b046d0b9ce09c1dd47f57
skhiearth <skhiearth@gmail.com>
2019-12-21 12:51:31 +0530

    infer on video

--- app.py
+++ app.py
@@ -29,3 +29,99 @@ def get_args():
     args = parser.parse_args()
 
     return args
+
+def infer_on_video(args):
+    args.c = convert_color(args.c)
+    args.ct = float(args.ct)
+    
+    ### Initialize the Inference Engine
+    plugin = Network()
+
+    ### Load the network model into the IE
+    plugin.load_model(args.m, args.d, CPU_EXTENSION)
+    net_input_shape = plugin.get_input_shape()
+
+    # Get and open video capture
+    cap = cv2.VideoCapture(args.i)
+    cap.open(args.i)
+
+    # Grab the shape of the input 
+    width = int(cap.get(3))
+    height = int(cap.get(4))
+
+    # Create a video writer for the output video
+    out = cv2.VideoWriter('out.mp4', 
+                          cv2.VideoWriter_fourcc('M','J','P','G'), 
+                          30, (width,height))
+    
+    # Process frames until the video ends, or process is exited
+    while cap.isOpened():
+        # Read the next frame
+        flag, frame = cap.read()
+        if not flag:
+            break
+        key_pressed = cv2.waitKey(60)
+
+        ### Pre-process the frame
+        p_frame = cv2.resize(frame, (net_input_shape[3], net_input_shape[2]))
+        p_frame = p_frame.transpose((2,0,1))
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+
+        ### Perform inference on the frame
+        plugin.async_inference(p_frame)
+
+        ### Get the output of inference
+        if plugin.wait() == 0:
+            result = plugin.extract_output()
+            ### Update the frame to include detected bounding boxes
+            frame = draw_boxes(frame, result, args, width, height)
+            # Write out the frame
+            out.write(frame)
+
+        # Write out the frame
+        out.write(frame)
+        # Break if escape key pressed
+        if key_pressed == 27:
+            break
+
+    # Release the out writer, capture, and destroy any OpenCV windows
+    out.release()
+    cap.release()
+    cv2.destroyAllWindows()
+
+    
+def draw_boxes(frame, result, args, width, height):
+    '''
+    Draw bounding boxes onto the frame.
+    '''
+    for box in result[0][0]: # Output shape is 1x1x100x7
+        conf = box[2]
+        if conf >= args.ct:
+            xmin = int(box[3] * width)
+            ymin = int(box[4] * height)
+            xmax = int(box[5] * width)
+            ymax = int(box[6] * height)
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), args.c, 1)
+    return frame
+
+
+def convert_color(color_string):
+    '''
+    Get the BGR value of the desired bounding box color.
+    Defaults to Blue if an invalid color is given.
+    '''
+    colors = {"BLUE": (255,0,0), "GREEN": (0,255,0), "RED": (0,0,255)}
+    out_color = colors.get(color_string)
+    if out_color:
+        return out_color
+    else:
+        return colors['BLUE']
+
+
+def main():
+    args = get_args()
+    infer_on_video(args)
+
+
+if __name__ == "__main__":
+    main()
