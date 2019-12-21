0e825a5ea7cb8e88e0d1fa9ba823ef4de7762a5c
skhiearth <skhiearth@gmail.com>
2019-12-21 12:54:05 +0530

    remove additional comments

--- inference.py
+++ inference.py
@@ -37,32 +37,20 @@ class Network:
 
 
     def get_input_shape(self):
-        '''
-        Gets the input shape of the network
-        '''
         return self.network.inputs[self.input_blob].shape
 
 
     def async_inference(self, image):
-        '''
-        Makes an asynchronous inference request, given an input image.
-        '''
         self.exec_network.start_async(request_id=0, 
                                       inputs={self.input_blob: image})
         return
 
 
     def wait(self):
-        '''
-        Checks the status of the inference request.
-        '''
         status = self.exec_network.requests[0].wait(-1)
         return status
 
 
     def extract_output(self):
-        '''
-        Returns a list of the results for the output layer of the network.
-        '''
         return self.exec_network.requests[0].outputs[self.output_blob]
 
