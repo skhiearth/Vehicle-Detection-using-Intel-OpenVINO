733acc1d0f21fde4a38658140a11cfa60d15003b
skhiearth <skhiearth@gmail.com>
2019-12-21 12:51:09 +0530

    arg parse

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 21 01:44:50 2019
+import argparse
+import cv2
+from inference import Network
 
-@author: skhiearth
-"""
+INPUT_STREAM = "test_video.mp4"
+CPU_EXTENSION = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so"
 
+def get_args():
+    parser = argparse.ArgumentParser("Run inference on an input video")
+    
+    # -- Create the descriptions for the commands
+    m_desc = "The location of the model XML file"
+    i_desc = "The location of the input file"
+    d_desc = "The device name, if not 'CPU'"
+    c_desc = "The color of the bounding boxes to draw; RED, GREEN or BLUE"
+    ct_desc = "The confidence threshold to use with the bounding boxes"
+
+    # -- Add required and optional groups
+    parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
+
+    # -- Create the arguments
+    required.add_argument("-m", help=m_desc, required=True)
+    optional.add_argument("-i", help=i_desc, default="test_video.mp4")
+    optional.add_argument("-d", help=d_desc, default='CPU')
+    optional.add_argument("-c", help=c_desc, default='BLUE')
+    optional.add_argument("-ct", help=ct_desc, default=0.5)
+    args = parser.parse_args()
+
+    return args
